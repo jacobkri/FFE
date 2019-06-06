b517e44f9c20e2c3b6b4919cc38626e2bf65cfc5
jacobkri <jacobseated@gmail.com>
2019-06-07 01:32:24 +0200

    Created a beautiful burger menu for mobile devices. This is probably my final commit on this project.

--- runner.js
+++ runner.js
@@ -1,12 +1,46 @@
 
+let burgerOpen = false;
 
 document.addEventListener("DOMContentLoaded", main);
 
-
 function main() {
-
+  document.querySelector("#nav_burger").addEventListener("click", toggleMenu);
 }
 
+function toggleMenu() {
+  let footerE = document.querySelector("footer");
+  let ffe_mainE = document.querySelector("#ffe_main");
+  let navMenuE = document.querySelector("#nav_menu");
+  let burger_presenterE = document.querySelector("#burger_presenter");
+  let burgerliE = document.querySelectorAll("#nav_menu ol li");
+
+  if (burgerOpen == false) {
+    burgerOpen = true;
+
+    ffe_mainE.style.display = 'none';
+    footerE.style.display = 'none';
+    burger_presenterE.style.display = 'block';
+
+    burgerliE.forEach(function (element) {
+      element.firstChild.addEventListener("mouseover", function (event) {
+        let background = this.getAttribute("data-bgimg-url");
+        burger_presenterE.style.backgroundImage = "url('" + background + "')";
+      });
+      element.firstChild.addEventListener("mouseout", function (event) {
+        burger_presenterE.style.backgroundImage = "none";
+      });
+    });
+    navMenuE.className = "show_menu";
+  } else {
+    burgerOpen = false;
+    ffe_mainE.style.display = 'block';
+    footerE.style.display = 'block';
+    navMenuE.className = "nav_standard";
+    burger_presenterE.style.display = 'none';
+
+  }
+
+}
 
 /*
 function main() {
--- templates/default/navigation_template.php
+++ templates/default/navigation_template.php
@@ -4,7 +4,7 @@ $requested_uri_clean = strtok($_SERVER["REQUEST_URI"],'?');
 
 // The container element for multiple units
 $navigation_container = <<<LOADTEMPLATE
-<nav>
+<nav id="nav_menu" class="nav_standard">
 <button id="nav_burger">☰</button>
 <div id="burger_presenter">
   <!-- Contains a preview of the page-title and thumbnail (if any) -->
